56f6e646ce1676022e11f7143f5f5e6f3836bb8f
Safia Rizwan <shajarizwan@gmail.com>
2025-04-23 08:14:48 -0400

    fixed bug where download-CSV button was dissapearing after clicking the 'show top 5' button

--- js/plots.js
+++ js/plots.js
@@ -520,6 +520,7 @@ function createTop5ToggleButton(){
       button.textContent = "Click to show top 5 metadata candidates only"
       showingTop5 = false
       makeLargeGrid()
+      makeExportButton()
       metaInput.checked = true;
       hazardInput.checked = false;
       if (hasMS2){MS2Input.checked = false;}
@@ -551,6 +552,7 @@ function createTop5ToggleButton(){
       button.textContent = "Showing Top 5 metadata candidates. Click to show all";
       showingTop5 = true
       makeLargeGrid()
+      makeExportButton()
       metaInput.checked = true;
       hazardInput.checked = false;
       if (hasMS2){MS2Input.checked = false;}
