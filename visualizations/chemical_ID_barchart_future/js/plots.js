4b7b57593cd37f629a7e7691cfb88ac4db243e6e
Chao <chao.alex@epa.gov>
2025-09-22 13:32:21 -0400

    Switch barchart input data to full data set with MS2

--- visualizations/chemical_ID_barchart_future/js/plots.js
+++ visualizations/chemical_ID_barchart_future/js/plots.js
@@ -2650,8 +2650,8 @@ screenshotButton.addEventListener('click', () => {
 
 // ======= CALL MAIN FUNCTION ==================================================================================================
 // const dataPath = "./data/short_test_noMS2.csv";
-const dataPath = "./data/short_test_amen.csv";
+// const dataPath = "./data/short_test_amen.csv";
 // const dataPath = "./data/short_test_no_amen.csv";
-// const dataPath = "./data/WW2DW_Data_Analysis_file_5_with_MS2.csv";
+const dataPath = "./data/WW2DW_Data_Analysis_file_5_with_MS2.csv";
 // const dataPath = "./data/WW2DW_Data_Analysis_file_5_without_MS2.csv";
 generatePlots(dataPath);
